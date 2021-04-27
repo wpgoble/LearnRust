2e5cc3787625d56b0d6c8671640b5b6c3be842f8
William Goble <will.goble@gmail.com>
2021-04-27 13:23:25 -0400

    creates hello world with cargo

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/goble/Documents/LearnRust/projects/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/goble/Documents/LearnRust/projects/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/goble/Documents/LearnRust/projects/hello_cargo/target/debug/hello_cargo: /Users/goble/Documents/LearnRust/projects/hello_cargo/src/main.rs
