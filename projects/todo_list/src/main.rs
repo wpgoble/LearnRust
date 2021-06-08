85d98561b84c4f0aed0bd3d72a1c9f920d270554
William Goble <will.goble@gmail.com>
2021-06-08 18:23:21 -0500

    adds ability to add items to file

--- projects/todo_list/src/main.rs
+++ projects/todo_list/src/main.rs
@@ -15,21 +15,53 @@ impl TodoItem {
 	}
 }
 
+struct TodoList {
+	list: Vec<TodoItem>
+}
+
+impl TodoList {
+	fn new() -> TodoList {
+		return TodoList { list: Vec::new()};
+	}
+
+	fn add_to_list(&mut self, name: String) {
+		let item = TodoItem::new(name);
+		self.list.push(item);
+	}
+
+	fn print(&self){
+		for item in &self.list {
+			println!("[{}] - {}", item.status, item.name);
+		}
+	}
+}
+
+enum Command {
+	Get,
+	Add(String)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 	
-	let cmd = &args[1];
-	// creating the first item in the todo list
-	let todo_item_1 = TodoItem::new("This is a test".to_string());
-	let todo_item_2 = TodoItem::new("Here is another one".to_string());
+	let command = match args[1].as_str() {
+		"get" => Command::Get,
+		"add" => Command::Add(args[2].clone()),
+		_ => panic!("You must provide a proper command")
+	};
 	
 	// our actual list of tasks
-	let todo_list = vec![todo_item_1, todo_item_2];
+	let mut todo_list = TodoList::new();
+	
+	// creating the first item in the todo list
+	todo_list.add_to_list("This is the first item".to_string());
+	todo_list.add_to_list("This is a second item".to_string());
 
-	if cmd == "get" {
-		println!("Get command called");
-		for item in todo_list {
-			println!("[{}] - {}", item.status, item.name); 
+	match command {
+		Command::Get  => todo_list.print(),
+		Command::Add(task) => {
+			todo_list.add_to_list(task.to_string());
+			todo_list.print();
 		}
 	}
 }
