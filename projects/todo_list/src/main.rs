518163d7d58a3cf9488736a4632de278a3dd57c1
William Goble <will.goble@gmail.com>
2021-06-08 19:41:08 -0500

    adds features for add, mark complete. and remove tasks

--- projects/todo_list/src/main.rs
+++ projects/todo_list/src/main.rs
@@ -30,15 +30,30 @@ impl TodoList {
 	}
 
 	fn print(&self){
-		for item in &self.list {
-			println!("[{}] - {}", item.status, item.name);
+		for (index, item) in self.list.iter().enumerate() {
+			println!("{} [{}] - {}", index, item.status, item.name);
 		}
+	}
+
+	fn mark_done(&mut self, index:usize) {
+		if self.list[index].status == ' ' {
+			self.list[index].status = 'x';
+		}
+		else {
+			self.list[index].status = ' ';
+		}
+	}
+
+	fn remove_task(&mut self, index:usize) {
+		self.list.remove(index);
 	}
 }
 
 enum Command {
 	Get,
-	Add(String)
+	Add(String),
+	Done(usize),
+	Remove(usize)
 }
 
 fn main() {
@@ -47,6 +62,8 @@ fn main() {
 	let command = match args[1].as_str() {
 		"get" => Command::Get,
 		"add" => Command::Add(args[2].clone()),
+		"done" => Command::Done(args[2].parse().expect("error converting to integert")),
+		"remove" => Command::Remove(args[2].parse().expect("error deleting task")),
 		_ => panic!("You must provide a proper command")
 	};
 	
@@ -56,11 +73,20 @@ fn main() {
 	// creating the first item in the todo list
 	todo_list.add_to_list("This is the first item".to_string());
 	todo_list.add_to_list("This is a second item".to_string());
+	todo_list.mark_done(1);
 
 	match command {
 		Command::Get  => todo_list.print(),
 		Command::Add(task) => {
 			todo_list.add_to_list(task.to_string());
+			todo_list.print();
+		},
+		Command::Done(index) => {
+			todo_list.mark_done(index);
+			todo_list.print();
+		},
+		Command::Remove(index) => {
+			todo_list.remove_task(index);
 			todo_list.print();
 		}
 	}
