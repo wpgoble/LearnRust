0c77748e4f3cf3e6dfb656da73fc94d1aac1dcf5
William Goble <will.goble@gmail.com>
2021-06-17 21:07:33 -0500

    wraps up chapter 3

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -24,4 +24,33 @@ fn main() {
 
     // in the above example we transform spaces 
     // from a string to a number
+
+    // variable types:
+    // Scalar types"
+    //      Integer Types: a number without a fractional component.
+    //          signed:   i8, i16, i32, i64, i128, isize
+    //          unsigned: u8, u16, u32, u64, u128, usize
+    //      Floating Point: 
+    let x = 2.0;        // default is f64
+    let y: f32 = 3.0;   // f32
+
+    // Numeric Operations
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let remainder = 43 % 5;
+    //      Booleans: 
+    let t = true;
+    let f: bool = false;
+    //      Characters
+
+    // Compound types
+    // Tuple: A general way of grouping together
+    // a number of values with a variety of types 
+    // into one type
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of x: {}, y: {}, z: {}", x, y, z);
 }
