5535b5318eda0c0db6f298cfd1d2428510b00598
William Goble <will.goble@gmail.com>
2021-07-21 18:51:06 -0500

    text example from Rust in practice book

---
+++ projects/hello2/target/debug/deps/hello2.d
@@ -0,0 +1,5 @@
+/Volumes/Work Space/LearnRust/projects/hello2/target/debug/deps/hello2: src/main.rs
+
+/Volumes/Work Space/LearnRust/projects/hello2/target/debug/deps/hello2.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello2/target/debug/hello2.d
@@ -0,0 +1,1 @@
+/Volumes/Work\ Space/LearnRust/projects/hello2/target/debug/hello2: /Volumes/Work\ Space/LearnRust/projects/hello2/src/main.rs
